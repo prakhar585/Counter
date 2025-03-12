f7b1da1ccea0f9c766d3ae1e8f0d7087ee7487c6
unknown <prakharchaturvedi9860@gmail.com>
2025-03-12 12:50:29 +0530

    completed all the basic requirements waiting for it to be tested

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+
+  const [valueOfCount , setValueOfCount ] = useState(0);
+
+  const handleDecrement=()=>{
+    setValueOfCount((prevValue)=>prevValue-1);
+  }
+
+  const handleIncrement=()=>{
+    setValueOfCount((prevValue)=>prevValue+1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counter App</h1>
+      Count: {valueOfCount}
+      <br></br>
+      <button onClick={handleIncrement}>increment</button>
+      <button onClick={handleDecrement}>decrement</button>
     </div>
   );
 }
